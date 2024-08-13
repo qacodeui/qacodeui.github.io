0204332b064953c50b00222aeff17489894b5197
Lyne <50180511+lmorissette@users.noreply.github.com>
2024-08-13 10:25:38 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/34708/14566/Zop0Tg45CEWrP0yxon7QUJZAYCjdFCDr/DI.WebPush-sw-lib.js');
+importScripts('https://docker.didev.ca/T/OFC4/WPS/34708/14566/Zop0Tg45CEWrP0yxon7QUJZAYCjdFCDr/DI.WebPush-sw-lib.js');
